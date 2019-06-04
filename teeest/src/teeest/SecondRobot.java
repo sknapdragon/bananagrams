9fbf6fbf30e343bd58efd9caa17a29b61c3d98b2
sknapdragon <47836031+sknapdragon@users.noreply.github.com>
2019-06-04 14:46:27 -0500

    Adjusted parameters for robot

--- teeest/src/teeest/SecondRobot.java
+++ teeest/src/teeest/SecondRobot.java
@@ -4,6 +4,7 @@ import CustomSensor.HiTechnicIRSeekerV2CustomStr;
 import lejos.hardware.Button;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
@@ -15,32 +16,22 @@ import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.utility.Delay;
 
-public class ProofOfConcept {
-	/*
-	 * static RegulatedMotor ma = new EV3MediumRegulatedMotor(MotorPort.A); static
-	 * RegulatedMotor mb = new EV3MediumRegulatedMotor(MotorPort.B); static
-	 * RegulatedMotor mc = new EV3MediumRegulatedMotor(MotorPort.C); static Wheel
-	 * wheela = WheeledChassis.modelHolonomicWheel(ma, 61).polarPosition(30,70
-	 * ).gearRatio(1); static Wheel wheelb = WheeledChassis.modelHolonomicWheel(mb,
-	 * 61).polarPosition(150,70 ).gearRatio(1); static Wheel wheelc =
-	 * WheeledChassis.modelHolonomicWheel(mc, 61).polarPosition(270,70
-	 * ).gearRatio(1); static Chassis chassis = new WheeledChassis(new Wheel[] {
-	 * wheela, wheelb, wheelc }, WheeledChassis.TYPE_HOLONOMIC);
-	 */
-
+public class SecondRobot {
 	static RegulatedMotor ma = new EV3MediumRegulatedMotor(MotorPort.A);
-	static RegulatedMotor mb = new EV3MediumRegulatedMotor(MotorPort.B);
-	static RegulatedMotor mc = new EV3MediumRegulatedMotor(MotorPort.C);
-	static Wheel wheela = WheeledChassis.modelHolonomicWheel(ma, 61).polarPosition(30, 70).gearRatio(1);
-	static Wheel wheelb = WheeledChassis.modelHolonomicWheel(mb, 61).polarPosition(150, 70).gearRatio(1);
-	static Wheel wheelc = WheeledChassis.modelHolonomicWheel(mc, 61).polarPosition(270, 70).gearRatio(1);
+	static RegulatedMotor mb = new EV3LargeRegulatedMotor(MotorPort.B);
+	static RegulatedMotor mc = new EV3LargeRegulatedMotor(MotorPort.C);
+	static RegulatedMotor md = new EV3MediumRegulatedMotor(MotorPort.D);
+	
+	static Wheel wheela = WheeledChassis.modelHolonomicWheel(mc, 61).polarPosition(30, 70).gearRatio(1);
+	static Wheel wheelb = WheeledChassis.modelHolonomicWheel(mb, 61).polarPosition(150, 70).gearRatio(-1); //reversed wheel b
+	static Wheel wheelc = WheeledChassis.modelHolonomicWheel(ma, 61).polarPosition(270, 70).gearRatio(1);
 	static Chassis chassis = new WheeledChassis(new Wheel[] { wheela, wheelb, wheelc }, WheeledChassis.TYPE_HOLONOMIC);
 	static Port s1 = LocalEV3.get().getPort("S1");
 	static Port s2 = LocalEV3.get().getPort("S2");
 	static Port s3 = LocalEV3.get().getPort("S3");
 	static Port s4 = LocalEV3.get().getPort("S4");
 
-	static SensorModes irSeeker = new HiTechnicIRSeekerV2CustomStr(s1);
+	static SensorModes irSeeker = new HiTechnicIRSeekerV2CustomStr(s3);
 	static SampleProvider iranglePro = irSeeker.getMode("Modulated");
 	static float[] irAngles = new float[iranglePro.sampleSize()];
 
