7b8e82422f8f9b8f8247051dfe9a68d8d35ce044
sknapdragon <47836031+sknapdragon@users.noreply.github.com>
2019-07-06 02:42:14 -0500

    light update - bt experiments

---
+++ teeest/src/type1/BlueToothData.java
@@ -0,0 +1,12 @@
+package type1;
+
+public class BlueToothData {
+	public BlueToothData(int otrIrAngle, float othIrStr) {
+		this.otrIrAngle = otrIrAngle;
+		this.othIrStr = othIrStr;
+	}
+	
+	int otrIrAngle;
+	float othIrStr;
+	
+}
